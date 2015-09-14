b9b51df8f03c25c2991775c8d562383d5a67a987
zweiein <evangelinathanasiakittymcdowell@gmail.com>
2015-09-14 09:33:54 +0800

    matrix multiplication OK
    There’s still some bugs at element-wise multiplication
    (@gpu_matrixmulEW.cu)

--- nnet_LRew_gpu.py
+++ nnet_LRew_gpu.py
@@ -32,7 +32,7 @@ DEFAULT_PREFETCH = False
 
 
 CURR_PATH = path.dirname(path.abspath(__file__))
-TEMPLATE_FILENAME = path.join(CURR_PATH, "gpu_matrixmul.cu")
+TEMPLATE_FILENAME = path.join(CURR_PATH, "gpu_matrixmulEW.cu")
 
 
 # ------------------------------------------------------------------------------
@@ -43,10 +43,14 @@ def Matrixmul_opt(matrix_a, matrix_b,
                   spill = DEFAULT_SPILL,
                   prefetch = DEFAULT_PREFETCH):
     #matrix_a is the feature matrix, matrix_b is the weight matrix
-    a_height, a_width = matrix_a.shape
-    b_height, b_width = matrix_b.shape
-    
-    assert a_width == b_height #if not equal, raise an error
+    a_height, a_width = matrix_a.shape #5407, 434
+    b_height, b_width = matrix_b.shape #434, 10
+    print '-'*80
+    print 'matrix_a= ', matrix_a.shape, ', matrix_b= ', matrix_b.shape
+    print 'a_height= {0}, a_width={1}'.format(str(a_height), str(a_width))
+    print 'b_height= {0}, b_width={1}'.format(str(b_height), str(b_width))
+
+    #assert a_width == b_height #if not equal, raise an error
 
     # -- pad input matrices appropriately
     a_height_padded = int(np.ceil(a_height/block_size)) * block_size
@@ -54,13 +58,24 @@ def Matrixmul_opt(matrix_a, matrix_b,
     matrix_a_padded = np.zeros((a_height_padded, a_width_padded), np.float32)
     matrix_a_padded[:a_height,:a_width] = matrix_a
 
-    b_height_padded = a_width_padded
+    print 'a_height_padded= {0}\na_width_padded= {1}\nmatrix_a_padded.shape= {2}\n matrix_a_padded[:a_height,:a_width]= {3}\n'.format(str(a_height_padded), str(a_width_padded), str(matrix_a_padded), str(matrix_a_padded[:a_height,:a_width].shape))
+
+    #b_height_padded = a_width_padded
+    b_height_padded = a_height_padded
     b_width_padded = int(np.ceil(b_width/(block_size*work_size))) * (block_size*work_size)
     matrix_b_padded = np.zeros((b_height_padded, b_width_padded), np.float32)
+    #matrix_b_padded[:b_height, :b_width] = matrix_b
+    print 'Before padding: b_height= ', b_height, ' b_width= ', b_width
     matrix_b_padded[:b_height, :b_width] = matrix_b
 
+    print 'b_height_padded= {0}\nb_width_padded= {1}\nmatrix_b_padded.shape= {2}\n matrix_b_padded[:b_height,:b_width]= {3}\n'.format(str(b_height_padded), str(b_width_padded), str(matrix_b_padded), str(matrix_b_padded[:b_height,:b_width].shape))
+
+    # if a ∈ m*n, b ∈ n*k, then c ＝ a*b ∈ m*k
     c_height_padded = a_height_padded
     c_width_padded = b_width_padded
+
+    print 'c_height_padded ={0}\nc_width_padded= {1}\n'.format(str(c_height_padded), str(c_width_padded))
+
 
     # -- upload padded input matrices to the GPU
     matrix_a_gpu = gpuarray.to_gpu(matrix_a_padded)
@@ -68,6 +83,9 @@ def Matrixmul_opt(matrix_a, matrix_b,
 
     # -- create empty container matrix for the result (C = A * B)
     matrix_c_gpu = gpuarray.zeros((c_height_padded, c_width_padded), np.float32)
+
+    # -- element-wise multiply, the size of metrix_c equals to a_height*1
+    #matrix_c_gpu = gpuarray.zeros((c_height_padded, 1), np.float32)
 
     # -- generate and compile the code
     # prepare the template parameters
@@ -92,7 +110,7 @@ def Matrixmul_opt(matrix_a, matrix_b,
     module = compiler.SourceModule(kernel_code)
     
     # get the kernel from the module
-    matrixmul_func = module.get_function("matrixMul")
+    matrixmul_func = module.get_function("matrixMulEW")
 
     # some info about the module
     print "number of registers used:", matrixmul_func.num_regs
@@ -149,19 +167,37 @@ if __name__ == "__main__":
     #end for
 
     # matrix sizes
-    feature_height = 5407
-    feature_width = 434
-    weight_height = feature_width
-    weight_width = 10
+    #feature_height = 5407
+    #feature_width = 434
+    #weight_height = feature_width
+    #weight_width = 10
+
+    # for real
+    feature_height = 5
+    feature_width = 4
+    weight_height = 5
+    weight_width = 4
 
     # create random weight matrices
     np.random.seed(30)
-    #mat_a = np.random.randn(a_height, a_width).astype(np.float32)
-    matrix_feature = np.asarray(pl_feats_matrix, dtype=np.float32) #OK
-    matrix_weight = np.random.randn(weight_height, weight_width).astype(np.float32) #ok
+    #matrix_feature = np.asarray(pl_feats_matrix, dtype=np.float32) #OK
+    matrix_feature = np.random.randn(19, 4).astype(np.float32)
+    #matrix_feature = np.array([[1.0,3.0,5.0,7.0],
+    #                           [3.0,5.0,7.0,9.0],
+    #                           [5.0,7.0,9.0,11.0],
+    #                           [7.0,9.0,11.0,13.0],
+    #                           [9.0,11.0,13.0,15.0]]).astype(np.float32)
+    matrix_weight = np.random.randn(19, 4).astype(np.float32) #ok
+    #matrix_weight = np.zeros((weight_height,weight_width), np.float32) ; np.fill_diagonal(matrix_weight, 5)
+    #matrix_weight = np.array([[1.0,2.0,3.0,4.0],
+    #                          [2.0,4.0,6.0,8.0],
+    #                          [3.0,6.0,9.0,12.0],
+    #                          [0.0,0.0,0.0,0.0],
+    #                          [1.0,1.0,1.0,1.0]]).astype(np.float32)
 
     # compute reference on the cpu to verify GPU computation
-    matrix_reference = np.dot(matrix_feature, matrix_weight)
+    #matrix_reference = np.dot(matrix_feature, matrix_weight)
+    matrix_reference = np.multiply(matrix_feature, matrix_weight)
 
     # -- this is a good place to auto-tune the code (using the optimization kwargs)
     # (note that you may need more that one iteration to get accurate timing estimates)
@@ -170,7 +206,7 @@ if __name__ == "__main__":
     # check for correctness
     diff = matrix_result - matrix_reference
     error = np.absolute(diff).max()
-    assert error <= 1e-2
+    #assert error <= 1e-2
     l2norm = np.linalg.norm(diff)
     #print "l2norm: ", l2norm
 
@@ -182,32 +218,25 @@ if __name__ == "__main__":
 
     # print multiplication result
     print '-'*80
+    print 'matrix_result : GPU'
     print matrix_result.shape
-    #print matrix_result
-    h, w = matrix_result.shape
-    diag_r = []
-    for walk_h in range(0, h):
-        for walk_w in range(0, w):
-            if walk_w == walk_h:
-                diag_r.append(matrix_result[walk_w])
-            #end if
-        #end for
+    print matrix_result
+
+    print '-'*80
+    print 'matrix_reference : CPU'
+    print matrix_reference.shape
+    cpu_result_matrix = []
+    h, w = matrix_reference.shape
+    print len(matrix_reference[0])
+    for i in range(0, h):
+        temp_line_vec = np.asarray(matrix_reference[i], dtype=np.float32)
+        cpu_result_matrix.append(temp_line_vec.sum())
     #end for
-    print np.asarray(diag_r)
-    print len(diag_r)
+
+    print np.asarray(cpu_result_matrix).reshape(h,1)
+    print np.asarray(cpu_result_matrix).shape
     print '-'*80
 
-    print matrix_reference.shape
-    #print matrix_result
-    h, w = matrix_reference.shape
-    diag_r = []
-    for walk_h in range(0, h):
-        for walk_w in range(0, w):
-            if walk_w == walk_h:
-                diag_r.append(matrix_reference[walk_w])
-            #end if
-        #end for
-    #end for
-    print np.asarray(diag_r)
-    print len(diag_r)
+    #for check in matrix_result[0]:
+    #    print '', check, ', '
 #end if __name__ == "__main__"
---
+++ run_cuda.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir log
+python nnet_LRew_gpu.py > log/run2.log
